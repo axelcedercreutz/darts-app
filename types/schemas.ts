80d345fd078048da5cbc2f4c67b9f0798cdbe368
Benjamin Kraatz <benjaminkraatz@yahoo.com>
2024-05-29 15:47:18 +0200

    add input validation to login/register

--- types/schemas.ts
+++ types/schemas.ts
@@ -22,3 +22,33 @@ export const ParticipantSchema = z.object({
 	profile_id: z.string().min(1),
 	game_id: z.string().min(1),
 });
+
+/**
+ * Schema for validating login data.
+ *
+ * The password requirement is set to a minimum of 6 characters
+ * and should be revised to meet specific security requirements.
+ */
+export const LoginSchema = z.object({
+	email: z.string().email(),
+	// TODO: rethink the password requirements here!
+	password: z.string().min(6),
+});
+
+/**
+ * Schema for validating registration data.
+ *
+ * The password requirement is set to a minimum of 6 characters
+ * and should be revised to meet specific security requirements.
+ *
+ * Also, the registration data should include a password confirmation field.
+ *
+ * **NOTE: registration currently grabs the `origin` from the header, which is _not_ part of the validation schema**.
+ * So basically we can use the `LoginSchema` for registration as well,
+ * but there might be a password confirmation field later
+ * or other fields that are not part of the login schema, like the origin.
+ */
+export const RegisterSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+});
