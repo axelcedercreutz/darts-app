516aee2262133b115496d620fff1fbf1de04be61
Axel Cedercreutz <axel.cedercreutz@gmail.com>
2024-06-01 11:16:12 +0300

    testing broken lint & tsc

--- types/common.ts
+++ types/common.ts
@@ -1,7 +1,9 @@
 import { Tables } from './supabase';
 
+console.log('testing')
+
 export type Profile = Tables<'profiles'>;
 
 export type Throw = Tables<'throws'>;
 
-export type GameParticipantWithProfile = Tables<'game_participants'> & { profiles: Profile };
+export type GameParticipantWithProfile = Tables<'game_partdddicipants'> & { profiles: Profile };
