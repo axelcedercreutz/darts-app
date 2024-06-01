c4cdcc02d57b10cdab8fe20f3f7d191c216eaaf5
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2024-06-01 10:59:37 +0300

    Add GH Actions Workflow for lint and tsc (#11)
    Ths PR adds the required GH action for the lint and tsc to run when a PR
    is opened / ready for review.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Leaderboard from "@/components/Leaderboard";
+import Leaderboard from '@/components/Leaderboard';
 
 export default async function Index() {
-  return (
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl py-3 px-3">
-          <Leaderboard />
-      </div>
-  );
+	return (
+		<div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl py-3 px-3">
+			<Leaderboard />
+		</div>
+	);
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,19 @@
 const Footer = () => {
-    return (
-        <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Made with ❤️ by the people at {" "}
-          <a
-            href="https://twice.market/?utm_source=darts-app&utm_medium=template&utm_term=darts-app"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            TWICE
-          </a>
-        </p>
-      </footer>
-    )
-}
+	return (
+		<footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+			<p>
+				Made with ❤️ by the people at{' '}
+				<a
+					href="https://twice.market/?utm_source=darts-app&utm_medium=template&utm_term=darts-app"
+					target="_blank"
+					className="font-bold hover:underline"
+					rel="noreferrer"
+				>
+					TWICE
+				</a>
+			</p>
+		</footer>
+	);
+};
 
 export default Footer;
